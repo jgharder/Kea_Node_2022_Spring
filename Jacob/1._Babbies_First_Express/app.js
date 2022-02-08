9e9b34e8219312ee4ecfcff90b1f4874318afa33
JGHarder <Jacob.gade.harder@gmail.com>
2022-02-08 11:39:59 +0100

    added app.js

--- Jacob/1._Babbies_First_Express/app.js
+++ Jacob/1._Babbies_First_Express/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-function callBackFunction()
 
-app.get("/");
+app.get("/", (req, res) => {
+  res.send({ message: "Hi there, Stranger" });
+});
+
+app.get("/about", (req, res) => {
+    res.send({ message: "This is a place for Movies" });
+  });
+
+
+
+app.post("/", (req, res) => {
+    res.send(req.body);
+});
+
+app.post("/opinion", (req, res) => {
+    res.send(req.body);
+});
 
 app.listen(8080);
-
